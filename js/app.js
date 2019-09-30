af3c8d6c742aedeb9a020cfffaa7f50ceb7c83b3
florian <florian@futureflow.io>
2019-09-30 22:27:21 +0800

    app v1

---
+++ js/app.js
@@ -0,0 +1,218 @@
+var mattress = [
+  {
+    size: 'SG-Single',
+    geo: 'SG',
+    type: 'single',
+    sizeWidth: 91,
+    sizeLength: 190,
+    color: 'rgba( 237, 41, 57, 0.2)',
+    source: 'https://www.hipvan.com/products/levitate-mattress'
+  },
+  {
+    size: 'SG-Queen',
+    geo: 'SG',
+    type: 'queen',
+    sizeWidth: 152,
+    sizeLength: 190,
+    color: 'rgba( 237, 41, 57, 0.2)',
+    source: 'https://www.hipvan.com/products/levitate-mattress'
+  },
+  {
+    size: 'SG-King',
+    geo: 'SG',
+    type: 'king',
+    sizeWidth: 183,
+    sizeLength: 190,
+    color: 'rgba( 237, 41, 57, 0.2)',
+    source: 'https://www.hipvan.com/products/levitate-mattress'
+  },
+  {
+    size: 'Europe-Single',
+    geo: 'Europe',
+    type: 'single',
+    sizeWidth: 90,
+    sizeLength: 200,
+    color: 'rgba( 0,51, 153,0.2)',
+  },
+  {
+    size: 'Europe-Queen',
+    geo: 'Europe',
+    type: 'queen',
+    sizeWidth: 160,
+    sizeLength: 200,
+    color: 'rgba( 0,51, 153,0.2)',
+  },
+  {
+    size: 'Europe-King',
+    geo: 'Europe',
+    type: 'king',
+    sizeWidth: 180,
+    sizeLength: 200,
+    color: 'rgba( 0,51, 153,0.2)',
+  },
+  {
+    size: 'US-Single',
+    geo: 'US',
+    type: 'single',
+    sizeWidth: 92,
+    sizeLength: 203,
+    color: 'rgba( 191,10,48,0.2 )'
+  },
+  {
+    size: 'US-Queen',
+    geo: 'US',
+    type: 'queen',
+    sizeWidth: 153,
+    sizeLength: 203,
+    color: 'rgba( 191,10,48,0.2 )',
+  },
+  {
+    size: 'US-King',
+    geo: 'US',
+    type: 'king',
+    sizeWidth: 183,
+    sizeLength: 203,
+    color: 'rgba( 191,10,48,0.2 )',
+  },
+  {
+    size: 'US-California King',
+    geo: 'US-California',
+    type: 'king',
+    sizeWidth: 183,
+    sizeLength: 213,
+    color: 'rgba( 191,10,48,0.2 )',
+  },
+]
+
+let containerElement = document.querySelector( '#mattressContainer' )
+let listingElement = document.querySelector( '#listing' )
+let geoListingElement = document.querySelector( '#geo' )
+let typeListingElement = document.querySelector( '#type' )
+
+let createdGeo = []
+let createdType = []
+
+// Create list of mattress
+mattress.forEach( function( oneData ){
+  console.log( oneData )
+  let elementId = oneData.size.replace( ' ', '.' )
+  oneData.elementId = elementId
+
+  let thisMattress = document.createElement( 'div' )
+  thisMattress.classList.add( 'oneSize' )
+  thisMattress.classList.add( elementId )
+  thisMattress.setAttribute( 'geo', oneData.geo )
+  thisMattress.setAttribute( 'type', oneData.type )
+  thisMattress.style.width = oneData.sizeWidth + 'px'
+  thisMattress.style.height = oneData.sizeLength + 'px'
+  thisMattress.style.backgroundColor = oneData.color
+  console.log( thisMattress )
+  containerElement.appendChild( thisMattress )
+
+  let thisListing = document.createElement( 'li' )
+  thisListing.classList.add( elementId )
+  thisListing.setAttribute( 'mattressId', elementId )
+  thisListing.innerText = oneData.size.replace( '-', ' ' ) + ' ' + mattressSizeText( oneData )
+  listingElement.appendChild( thisListing )
+
+  // Create Geo Listing
+  if( !createdGeo.includes( oneData.geo ) ){
+    createdGeo.push( oneData.geo )
+    let thisGeo = document.createElement( 'li' )
+    thisGeo.classList.add( oneData.geo )
+    thisGeo.setAttribute( 'geo', oneData.geo )
+    thisGeo.innerText = oneData.geo
+    geoListingElement.appendChild( thisGeo )
+  }
+
+  // Create Type listing
+  if( !createdType.includes( oneData.type ) ){
+    createdType.push( oneData.type )
+    let thisType = document.createElement( 'li' )
+    thisType.classList.add( oneData.type )
+    thisType.setAttribute( 'type', oneData.type )
+    thisType.innerText = oneData.type
+    typeListingElement.appendChild( thisType )
+  }
+
+
+
+} )
+
+
+// Selector of mattress
+let listingElements = document.querySelectorAll( '#listing li' )
+listingElements.forEach( function( oneListing ){
+  oneListing.addEventListener( 'click', function( oneElement ){
+    let targetId = oneElement.target.getAttribute( 'mattressId' )
+
+    let targetMattressId = '.oneSize.' + targetId
+    let targetMattress = document.querySelector( targetMattressId )
+    // targetMattress.style.border = '3px solid red'
+
+    highlightOne( targetId )
+  } )
+} )
+
+function removeHighlight(){
+  document.querySelectorAll( '.oneSize' ).forEach( function( oneSize ){
+    oneSize.classList.remove( 'offHighlight' )
+    oneSize.classList.remove( 'highlight' )
+    oneSize.innerText = ''
+  } )
+}
+function highlightOne( targetId ){
+  removeHighlight()
+
+  let elementData = mattress.find( function( one ){
+    return one.elementId === targetId
+  } )
+
+  document.querySelectorAll( '.oneSize' ).forEach( function( oneSize ){
+    if( !oneSize.classList.contains( targetId ) ){
+      oneSize.classList.add( 'offHighlight' )
+    }else{
+      oneSize.classList.add( 'highlight' )
+      oneSize.innerText = mattressSizeText( elementData )
+    }
+  } )
+}
+
+
+function mattressSizeText( data ){
+  return data.sizeWidth + 'cm * ' + data.sizeLength + 'cm'
+}
+
+
+
+// Selector of Geo
+let geoElements = document.querySelectorAll( '#geo li' )
+geoElements.forEach( function( oneGeo ){
+  oneGeo.addEventListener( 'click', function( oneGeoClickElement ){
+    let clickedGeoId = oneGeoClickElement.target.getAttribute( 'geo' )
+    console.log( 'clickedGeoId', clickedGeoId )
+    highlightBy( 'geo', clickedGeoId )
+  } )
+} )
+
+// Selector of Type
+let typeElements = document.querySelectorAll( '#type li' )
+typeElements.forEach( function( oneType ){
+  oneType.addEventListener( 'click', function( oneTypeClickElement ){
+    let clickedTypeId = oneTypeClickElement.target.getAttribute( 'type' )
+    highlightBy( 'type', clickedTypeId )
+  } )
+} )
+
+
+function highlightBy( attributeType, value ){
+  removeHighlight()
+  document.querySelectorAll( '.oneSize' ).forEach( function( oneSize ){
+    let thisAttributeValue = oneSize.getAttribute( attributeType )
+    if( thisAttributeValue === value ){
+      oneSize.classList.add( 'highlight' )
+    }else{
+      oneSize.classList.add( 'offHighlight' )
+    }
+  } )
+}
